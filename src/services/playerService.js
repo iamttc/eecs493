c61d6f02fcf69f868e4b67f6b23b1d7d65a53292
Peter James Flanagan <pjflanagan1@gmail.com>
2017-11-19 16:43:45 -0500

    velocity

--- src/services/playerService.js
+++ src/services/playerService.js
@@ -1,6 +1,15 @@
 import { $, socket } from './baseService';
 import { updatePlayerLocation, updateContent } from '../redux/actions';
 
+//var R = 10;
+var CENTER = {
+  X: window.innerWidth/2,
+  Y: window.innerHeight/2
+}
+
+var W = window.innerWidth;
+var H = window.innerHeight;
+
 export class PlayerService {
 
   constructor() {
@@ -19,7 +28,11 @@ export class PlayerService {
     // positioning
     this.top = 0;
     this.left = 0;
+    this.x = 0;
+    this.y = 0;
     this.rotation = 0;
+
+    this.velocity = 9;
   }
 
   initPlayerService() {
@@ -41,6 +54,22 @@ export class PlayerService {
    * continuously emit the new location of the player
    */
   updateLocation = () => {
+
+    var d = {
+      x: this.left - this.x,
+      y: this.top - this.y,
+    };
+
+    if(Math.abs(d.x) > 12 || Math.abs(d.y) > 12){
+      var h = Math.sqrt(Math.pow(d.x,2) + Math.pow(d.y,2));
+      var a = Math.atan2(d.x, d.y);
+
+      var v = h/100 * this.velocity;
+
+      this.top = (-v) * Math.cos(a) + this.top,
+      this.left = (-v) * Math.sin(a) + this.left
+    }
+
     socket.emit('update location', {
       playerId: this.playerId,
       position: {
@@ -69,21 +98,25 @@ export class PlayerService {
     $(window).keypress((e) => {
       switch(e.which) {
         case this.KEYS.up:
-          this.top -= 10;
+          this.y = this.top - 100;
           break;
         case this.KEYS.down:
-          this.top += 10;
+          this.y = this.top + 100;
           break;
         case this.KEYS.left:
-          this.left -= 10;
+          this.x = this.left - 100;
           break;
         case this.KEYS.right:
-          this.left += 10;
+          this.x = this.left + 100;
           break;
         case this.KEYS.spacebar:
         default:
           break;
       }
+    });
+    $(window).mousemove((e) => {
+      var r = Math.atan2(CENTER.Y-e.pageY, CENTER.X-e.pageX) - Math.PI/2;
+      this.rotation = r;
     });
   }
 }
