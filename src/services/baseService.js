6695a4256960bb6cd89d024e9f9fe55f2c9315b6
Trever Cullen <trevercullen@gmail.com>
2017-11-30 23:11:20 -0500

    backend deployment

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:8000');
-// const socket = openSocket('http://space-fighters.herokuapp.com');
+// const socket = openSocket('http://space-fighters-backend.herokuapp.com');
 
 export { $, socket };
